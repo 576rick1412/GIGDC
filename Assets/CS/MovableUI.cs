b3921f84b5fbd4e2c55ce8805d2365bafedc427a
576rick1412 <576rick1412@gmail.com>
2023-05-15 23:46:46 +0900

    드래그 시스템 제작중

---
+++ Assets/CS/MovableUI.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+// 헤더 드래그 앤 드롭에 의한 UI 이동
+public class MovableUI : MonoBehaviour, IPointerDownHandler, IDragHandler
+{
+    [SerializeField]
+    private Transform _targetTr; // 이동될 UI
+
+    private Vector2 _startingPoint;
+    private Vector2 _moveBegin;
+    private Vector2 _moveOffset;
+
+    private void Awake()
+    {
+        // 이동 대상 UI를 지정하지 않은 경우, 자동으로 부모로 초기화
+        if (_targetTr == null)
+            _targetTr = transform.parent;
+    }
+
+    // 드래그 시작 위치 지정
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+    {
+        _startingPoint = _targetTr.position;
+        _moveBegin = eventData.position;
+    }
+
+    // 드래그 : 마우스 커서 위치로 이동
+    void IDragHandler.OnDrag(PointerEventData eventData)
+    {
+        _moveOffset = eventData.position - _moveBegin;
+
+        if (_startingPoint.x + _moveOffset.x > (Screen.width / 2) + 1050) return;
+        if (_startingPoint.x + _moveOffset.x < (Screen.width / 2) - 1050) return;
+
+        if (_startingPoint.y + _moveOffset.y > (Screen.height / 2) + 700) return;
+        if (_startingPoint.y + _moveOffset.y < (Screen.height / 2) - 700) return;
+
+        // 이동
+        _targetTr.position = _startingPoint + _moveOffset;
+    }
+}
